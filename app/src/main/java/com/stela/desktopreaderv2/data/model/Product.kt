d39c6852a26c258d6fb0dbbaa9e8233efc77caae
Igor <gigor497@gmail.com>
2026-05-04 11:21:24 -0300

    Initial project settings

---
+++ app/src/main/java/com/stela/desktopreaderv2/data/model/Product.kt
@@ -0,0 +1,15 @@
+package com.stela.desktopreaderv2.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class Product(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val tagEpc: String,
+    val description: String,
+    val quantity: Int
+
+)

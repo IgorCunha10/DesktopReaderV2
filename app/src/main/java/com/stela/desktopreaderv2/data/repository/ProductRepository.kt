98ba021b3493aed123c626fb3898998d816571e8
Igor <gigor497@gmail.com>
2026-05-04 12:14:48 -0300

    Implemented ProductsRepository

--- app/src/main/java/com/stela/desktopreaderv2/data/repository/ProductRepository.kt
+++ app/src/main/java/com/stela/desktopreaderv2/data/repository/ProductRepository.kt
@@ -1,4 +1,15 @@
 package com.stela.desktopreaderv2.data.repository
 
-class ProductRepository {
+import com.stela.desktopreaderv2.data.local.ProductDao
+import com.stela.desktopreaderv2.data.model.Product
+import kotlinx.coroutines.flow.Flow
+
+class ProductRepository(private val dao: ProductDao) {
+
+    fun getAllProducts(): Flow<List<Product>> = dao.getAllProduct()
+    suspend fun getProductByTag(epc: String): Product? = dao.getProductByTag(epc)
+    suspend fun insertProduct(product: Product) = dao.insertProduct(product)
+    suspend fun deleteProduct(product: Product) = dao.deleteProduct(product)
+    suspend fun updateProduct(product: Product) = dao.updateProduct(product)
+
 }
